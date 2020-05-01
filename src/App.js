096330ce1398562035158c682531612fc4a8732e
Yu <ygao0018@gmail.com>
2020-05-01 22:12:21 +1000

    pass data between files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    //add a line
-    //
-
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Person name = "Gao Yu" age = "18"></Person>
+
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const person = (preps) => {
+    return (
+    <p>My name is {preps.name}, I am {preps.age} years old.</p>
+    )
+}
+
+
+export default person;
